efaf3d695c0c4266891a2a9af9aab83e856b1db5
Yaylyeav Safuan <Safuan-93@mail.ru>
2019-12-15 15:42:12 +0300

    удалены комментарии

--- hw5/addNewComment.php
+++ hw5/addNewComment.php
@@ -1,18 +1,12 @@
 <?php
-    if (!empty ($_POST['comment'])) { //Проверка на существование и на пустоту
+    if (!empty ($_POST['comment'])) {
         $comment = $_POST['comment'];
     } else {
         $comment = null;
     }
 
-    //получи содержимое файла в виде массива. каждая строка это отдельный элемент массива
     $comments = file(__DIR__.'/comments', FILE_IGNORE_NEW_LINES);
-    // Добавь элемент $comment в массив $comments
     $comments[] = $comment;
-    // объедини элементы массива в строку разделяя их символом перевода строки "\n"
-    $file = implode("\n", $comments); //var_dump($file);
-    // Запиши данные в файл comments
+    $file = implode("\n", $comments);
     file_put_contents(__DIR__ . '/comments', $file);
-
-    header ('Location: /hw5/guestBook.php'); // перенаправь на страницу Гостевой книги
-
+    header ('Location: /hw5/guestBook.php');
