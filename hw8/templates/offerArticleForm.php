43dd045fc78e8c1cc7fae27c64cf7628d39e22f4
Yaylyeav Safuan <Safuan-93@mail.ru>
2019-12-26 00:13:38 +0300

    удалена форма добавления статьи

--- hw8/templates/offerArticleForm.php
+++
@@ -1,7 +0,0 @@
-<form action="/hw7/addNewArticle.php" method="post" xmlns="http://www.w3.org/1999/html">
-    Заголовок новости: <br>
-    <input type="text" name="title"><br>
-    Текст новости:<br>
-    <input type="text" name="text"><br>
-    <button type="submit"> Опубликовать </button>
-</form>
